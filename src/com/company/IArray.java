28b2ecfbc4d4e1c3e1e56bcf449c966111dedbcd
lampvideo <alvk.klimov@gmail.com>
2022-04-02 21:48:40 +0300

    add interface

---
+++ src/com/company/IArray.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public interface IArray <T> {
+    T get(int index);
+    void put(T item);
+    void put(int pos, T item);
+    int size() ;
+
+    default boolean isEmpty() {
+        return false;
+    }
+}
